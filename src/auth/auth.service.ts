3e0aea685fad77c7a0d87d96a3f51ccec18ad932
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2022-09-10 13:43:29 -0400

    add fake users and auth validateUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { User, UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(
+    username: string,
+    password: string,
+  ): Promise<Omit<User, 'password'> | null> {
+    const user = await this.usersService.findOne(username);
+
+    if (user && user.password === password) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
